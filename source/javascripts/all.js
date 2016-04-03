5092345c2ff1d4a34d6a97a4318e029e6a775f5b
Jaclyn Perrone <jaclynperrone@Jaclyns-MacBook-Air.local>
2016-04-03 18:43:57 -0400

    Style overhaul. Build out rest of pages.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,19 @@
-// This is where it all goes :)
+$(function(){
+
+	$('.press-nav li').click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+
+		var selectedPress = $('.active a').attr('rel');
+		
+		if (selectedPress == 'interviews') {
+			$('#reviews').fadeOut('fast', function(){
+				$('#' + selectedPress).fadeIn();
+			});
+		} else {
+			$('#interviews').fadeOut('fast', function(){
+				$('#' + selectedPress).fadeIn();
+			});
+		}
+	});
+
+});
